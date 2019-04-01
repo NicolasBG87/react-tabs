128e3bc7a7edbfed28d177c11cf3fdc52c5e9433
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-04-01 20:25:19 +0200

    Updates

---
+++ precache-manifest.0052c268172a6f42db5a59cc8c08c589.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1741df148a1c29fc5975",
+    "url": "/react-tabs/static/js/runtime~main.f06cae23.js"
+  },
+  {
+    "revision": "e4334efc0e757027ea54",
+    "url": "/react-tabs/static/js/main.c520a297.chunk.js"
+  },
+  {
+    "revision": "9af43dfe8b14a37a48ec",
+    "url": "/react-tabs/static/js/2.f3e18cfd.chunk.js"
+  },
+  {
+    "revision": "96f499d022d541dd76caa8401badde8c",
+    "url": "/react-tabs/index.html"
+  }
+];
